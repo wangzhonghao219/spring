45b505eafc34f687f857b50dc9c6a9e92e353576
王中豪 <74975683+wangzhonghao219@users.noreply.github.com>
2024-03-22 04:55:29 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,19 +1,42 @@
 Bean
 //UserService.class ----> 无参数(***推断***)构造方法（反射）多种构造方法调用带注解的构造器（@2autowired） (deCreateBean) 站在spring的角度去思考 -------> 普通对象(obj) 
 //----------> 注入依赖(autowired)(属性填充)   ------>  初始化(spring的初始化 != JVM初始化) ----->  初始化后(post)AOP  ，需要spring事务的时候需要aop
-//------> (用) 代理对象 ------->  放入Map<BeanName,Bean对象>单例池  如果需要AOP，则MAP存放的是代理对象，如果不需要AOP则map存放的是普通对象
-
+//------> (用) 代理对象 ------->  放入Map<BeanName,Bean对象>单例池  如果需要AOP，则Map存放的是代理对象，如果不需要AOP则map存放的是普通对象
+//spring事务会给普通对象生成代理对象，从而产生bean
 
 //注入依赖(@autowired)是实现属性，初始化是实现方法,  *********************只有autowired注解才可以用到构造方法上  @autowired   ****************
 @Component
 public class UserService{
   private OrderService orderService;
 
+  @Autowired
+  private JdbcTemplate jdbctemplate；
+
+  @Transactional
+  public void test(){
+    jdbcTemplate.execute(sql:"insert into t1 values(6,6,4,4,'f')");  //此时因为是JdbcTemplate.execute方法，和spring没有关系，不在spring的控制之下，所以必须AOP，执行切面逻辑，将autocommit = true（自动提交） 改为  false，将sql变为在spring的控制之下
+    throw new NullPointerException();  //抛出异常就不可以回滚          //用的是spring创建的数据库连接
+    a();  //new UserService().a()  用的普通对象的方法
+    //AOP后，执行***spring事务***的切面逻辑
+    //解析注解,代理方法去解析
+    //新建一个数据库连接connection  ThrealLocal放置创建的连接   有一个扩展机制
+    //connection.autocommit = false;
+
+    //target.test   //jdbcTemplate conn 执行SQL
+    //conn.commit()  conn.rollback()
+  }
+  //*********方法的注解会不会被解析，会不会有用，看是什么对象调用的，如果是代理对象调用的则有用（spring事务触发），如果是普通对象调用的，则没有太大的关系
+  @Transactional(propagation = Propagation.NEVER)  //传播机制
+  public void a(){
+      jdbcTemplate.execute(sql:"insert into t1 values(2,2,4,4,'2')"); 
+  }
+  
+
   public UserService(OrderService orderService1){  //构造方法  OrderServiced对象来源自Map<BeanName, Bean对象>（byType，根据类型去找），Map没有则 ------->创建Bean对象（必须有Component注解），若无Component注解声明对象为Bean对象，则传递失败，spring也不返回null
     this.orderService = orderService1;             //若无Component注解声明对象为Bean对象，则传递失败，spring也不返回null,会报错，因为这是构造方法---> 强制依赖，*****注意循环依赖，加Lazy注解解决
     System.out.println(1);                        //hashmap<>  key唯一  value不唯一    Map<BeanName,Bean对象>  *******先ByType (如果找到多个) ---------> 再ByName*************,***如果只有一个Type的Bean对象，就不用再ByName了，有多个则必确明确指出BeanName
   }
-  public void test(){}
+  //public void test(){}//执行普通对象
 }
 
 
@@ -53,7 +76,7 @@ public class WzhAspect{
 //UserService代理对象，先生成代理类，即UserSerciceProxy类,***************但是spring不对代理对象进行依赖注入
 //UserService代理对象.test()方法
 //UserSerciceProxy对象==UserService代理对象------->UserService代理对象.target = 普通对象（一开始就创建出来的，代理对象代理的就是普通对象）
-class UserServiceProxy extends UserService {
+class UserServiceProxy extends UserService {   //为什么要继承，因为不继承用的时候不知道代理类的名字叫什么，CGLIB创建代理类有一定规则，每次运行不一样，所以只用关心UserService，不关心其代理类
 
   UserService target;
   //重写UserService类的test方法，AOP执行普通对象的逻辑之前额外执行一些逻辑  ==》加强版的test方法
